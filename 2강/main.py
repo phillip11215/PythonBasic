437219933f9551dd687528e0d5955ec9ccd86641
phillip11215 <95954765+phillip11215@users.noreply.github.com>
2021-12-18 16:44:15 +0900

    Create main.py

---
+++ 2강/main.py
@@ -0,0 +1,121 @@
+print("Hello World")
+print('Hello World')
+print('철수가 인사한다. "안녕?"')
+print("철수가 인사한다. \"안녕?\"")
+print('나의 \'첫 코딩\' 입니다')
+print("나의 '첫 코딩' 입니다")
+print("오늘은 , 일요일")
+print("오늘은", "일요일")
+a = 1
+print(a)
+a = 123
+print(a)
+a = 15614454651326541564894163245641567465416574654798654897456465125641567416547514654894
+print(a)
+a = 1.2
+print(a)
+a = -3
+print(a)
+a = 0o177
+print(a)
+a = 46546541657897485456561316857497987641561456456415631564789748945648974894564657897465456451453465499999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999
+b = 13164165478964165465498479564165415649874896641657465156456749864894894456945645645674987485749845641651521328948947859456465
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+a = 1165416541654654685999999999999999999999999999999999999999
+b = 9
+print(a ** b)
+a = 10000000
+b = 3
+print(a % b)
+a = 7
+b = 4
+print(a / b)
+a = 80
+b = 75
+c = 65
+d = a + b + c
+print("평균점수 :", d/3)
+a = 89
+b = 3
+print(a // b)
+print(a % b)
+print(23 % 2) # 1
+print("안녕하세요. 오늘의 날씨는 화창합니다")
+print('지나가던 한 사람이 물었다. "실례합니다. 여의도역으로 가는길이 어디입니까?" 질문 받은 사람이 대답했다. \" I don\'t know."')
+print('\   /\\')
+print(' ) ( \')')
+print('(  / )')
+print(' \(_)\\')
+A = 14
+B = 3
+print(A+B)
+print(A-B)
+print(A*B)
+print(A/B)
+print(A%B)
+A = 5
+B = 8
+C = 4
+print((A+B)%C)
+print((A%C)*(B%C)+1)
+A = 472*5
+B = 472*8
+C = 472*3
+print((A*1)+(B*10)+(C*100))
+age = 12
+print (age)
+공책 = 3
+필통 = 1
+교과서 = 1
+print(공책,필통,교과서)
+print("\"파이썬은 쉽다\"라고 '철수'가 말했어요.")
+print('"파이썬은 쉽다"라고 \'철수\'가 말했어요.')
+print("\"파이썬은 쉽다\"라고 \'철수\'가 말했어요.")
+print(""""파이썬는 쉽다"라고 '철수'가 말했어요.""")
+string = """오늘 날씨는 춥고
+나는 오늘 기쁘다."""
+print(string)
+hello = "안녕하세요!_오늘은_기분이_좋습니다."
+print(hello[2])
+hello = "\"안녕.\" 철수가 인사를 했다"
+print(hello[10])
+hello = "안녕하세요! 오늘은 기분이 좋습니다."
+print(hello[-6])
+hello = "Life is too short, You need Python"
+print(hello[0])
+print(hello[8])
+print(hello[2])
+print(hello[9])
+a = "Life is too short, You need Python"
+print(a[:34])
+number = "24가 2210"
+print(number[-4:])
+a = "우리 강아지 이름은 초코입니다"
+print(a[11:13])
+print("Hi"*3)
+print("Hi!"*3)
+print("-"*80)
+s1 = "python"
+s2 = "java "
+s3 = s1+s2
+print(s3*4)
+s1 = "좋은 아침입니다!"
+s2 = s1[0:5]+s1[-1]
+print(s2)
+name1 = "김철수"
+age1 = 10
+name2 = "홍길동"
+age2 = 13
+print("이름: %s 나이: %d" %(name1, age1))
+print("이름: %s  나이: %d" %(name2, age2))
+name = "김철수"
+age1 = 10
+name = "홍길동"
+age2 = 13
+print("이름: {0} 나이: {1}".format(name1, age1))
+print("이름: {0} 나이: {1}".format(name2, age2))
+date = "2021/09/21"
+print(date[:7])
